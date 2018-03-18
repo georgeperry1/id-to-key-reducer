b872c8c4b77714e1a5c64602c8bb575c7ee7e750
georgeperry1 <gmperry18@gmail.com>
2018-03-18 23:06:43 +0100

    test: test added for id as well as _id

---
+++ __mock__/mockArrayDataTwo.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const mockArrayDataTwo = [
+  {
+    name: 'Ben',
+    age: 47,
+    id: 1001
+  },
+  {
+    name: 'Aaron',
+    age: 27,
+    id: 1002
+  },
+  {
+    name: 'Brendan',
+    age: 25,
+    id: 1003
+  },
+  {
+    name: 'Sachin',
+    age: 25,
+    id: 1004
+  },
+  {
+    name: 'Ibrahim',
+    age: 59,
+    id: 1005
+  }
+];
+
+module.exports = mockArrayDataTwo;
--- test/index.spec.js
+++ test/index.spec.js
@@ -5,22 +5,27 @@ const expect = chai.expect;
 const should = chai.should;
 const assert = chai.assert;
 const mockArrayData = require('../__mock__/mockArrayData');
+const mockArrayDataTwo = require('../__mock__/mockArrayDataTwo');
 const mockObjectData = require('../__mock__/mockObjectData');
 const idToKeyTest = require('../index');
 
 describe('idToKey', () => {
   const mockDataOne = mockArrayData;
   const mockDataTwo = mockObjectData;
+  const mockDataThree = mockArrayDataTwo;
   const newDataOne = idToKeyTest(mockDataOne);
   const newDataTwo = idToKeyTest(mockDataTwo);
+  const newDataThree = idToKeyTest(mockDataThree);
 
   it('should return an Object', () => {
     expect(newDataOne).to.be.an('object');
+    expect(newDataThree).to.be.an('object');
   });
 
   it('should only accept an Array', () => {
     expect(newDataOne).to.not.be.an('error');
     expect(newDataTwo).to.be.an('error');
+    expect(newDataThree).to.not.be.an('error');
   });
 
   it('should return an Object with ids as keys', () => {
@@ -30,11 +35,20 @@ describe('idToKey', () => {
     expect(newDataOne).to.have.property('1004');
     expect(newDataOne).to.have.property('1005');
     expect(newDataOne).to.not.have.property('1009');
+    expect(newDataThree).to.have.property('1001');
+    expect(newDataThree).to.have.property('1002');
+    expect(newDataThree).to.have.property('1003');
+    expect(newDataThree).to.have.property('1004');
+    expect(newDataThree).to.have.property('1005');
+    expect(newDataThree).to.not.have.property('1009');
   });
 
   it('should return an Object with the key values as the original objects', () => {
     expect(newDataOne).to.have.nested.property('1001.name');
     expect(newDataOne).to.have.nested.property('1001.age');
     expect(newDataOne).to.not.have.nested.property('1001.height');
+    expect(newDataThree).to.have.nested.property('1001.name');
+    expect(newDataThree).to.have.nested.property('1001.age');
+    expect(newDataThree).to.not.have.nested.property('1001.height');
   });
 });
